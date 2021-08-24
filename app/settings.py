61c6b84dc4e811987ca72c73dfdce08f99d7481d
tavoxr <tavoxr23@gmail.com>
2021-08-23 18:52:50 -0600

    cors vue-ecomm

--- app/settings.py
+++ app/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
-    "https://drf-ecomm-api.herokuapp.com"
+    "https://vue-ecomm-tavox.netlify.app"
 ]
 
 MIDDLEWARE = [
